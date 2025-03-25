709c1ed6583866281757153dc58c929cf083ec53
sharondotchen <chen.sharo@northeastern.edu>
2025-03-25 10:49:59 -0400

    first build

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/city-explorer-class-exercise/',
   plugins: [svelte()],
+  build:{
+    outDir: 'docs',
+    emptyOutDir:true
+  }
 })
